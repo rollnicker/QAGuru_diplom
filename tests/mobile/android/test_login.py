e9542f3affa2faf76195d0c8895b1c29638e4f2a
Rolnik Boris <rollnicker@mail.ru>
2024-03-10 00:26:17 +0300

    Изменил scope на function

--- tests/mobile/android/test_login.py
+++ tests/mobile/android/test_login.py
@@ -2,7 +2,7 @@ import os
 
 from allure_commons._allure import step
 from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, have
+from selene import browser, have, be
 
 
 def test_login(android_mobile_management):
