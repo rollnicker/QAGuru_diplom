43b9c35fc7385ad3e673851b4d2c6928b75b4d92
Rolnik Boris <rollnicker@mail.ru>
2024-03-10 00:36:07 +0300

    Изменил scope на function

--- tests/mobile/android/test_add_to_favourites.py
+++ tests/mobile/android/test_add_to_favourites.py
@@ -5,7 +5,10 @@ from selene import browser, have, be
 
 def test_add_book_to_favourites(android_mobile_management):
     with step('закрыть выбор языка'):
-        browser.element('ru.litres.android:id/choosebutton').click()
+        if browser.element((AppiumBy.ID, 'ru.litres.android:id/choosebutton')).should(be.existing):
+            browser.element((AppiumBy.ID, 'ru.litres.android:id/choosebutton')).click()
+        else:
+            pass
     with step('закрыть крестик'):
         browser.element((AppiumBy.ID, 'ru.litres.android:id/circleButtonSubscriptionPaywallClose')).click()
     with step('открыть поиск'):
